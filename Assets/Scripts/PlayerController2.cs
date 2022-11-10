3000615e8aa9827d5cd33993d581d021c8cebca5
ifernandezAD <ifernandezad@gmail.com>
2022-11-10 17:06:28 +0100

    Protoype Maze
    Player movement corrected too

---
+++ Assets/Scripts/PlayerController2.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2 : MonoBehaviour
+{
+    private Rigidbody myRigid;
+
+    private Vector3 inputVector;
+    public float speed;
+
+    void Start()
+    {
+        myRigid = GetComponent<Rigidbody>();
+    }
+
+
+    void Update()
+    {
+        inputVector = new Vector3(Input.GetAxis("Horizontal") * speed, myRigid.velocity.y, Input.GetAxis("Vertical") * speed);
+        transform.LookAt(transform.position + new Vector3(inputVector.x, 0, inputVector.z));
+    }
+
+    private void FixedUpdate()
+    {
+        myRigid.velocity = inputVector;
+
+    }
+}
