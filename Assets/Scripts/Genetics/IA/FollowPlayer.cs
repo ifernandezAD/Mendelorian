3f9f916e7110dc146d047f008219964a7e78214b
ifernandezAD <ifernandezad@gmail.com>
2022-11-10 19:09:26 +0100

    Enemy follows the player Script

---
+++ Assets/Scripts/Genetics/IA/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private NavMeshAgent enemyNav;
+    public Transform target;
+
+    
+    void Start()
+    {
+        enemyNav = GetComponent<NavMeshAgent>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        enemyNav.SetDestination(target.position);
+    }
+}
